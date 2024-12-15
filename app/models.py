820572cba2b6dd629fc883ca78f44e455c491a2c
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-12-15 16:10:52 +0200

    created and run database

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class UserProfile(AbstractUser):
+    pass
+
+
+ARTICLE_STATUS = (
+    ("draft", "draft"),
+    ("inprogress", "in progress"),
+    ("published", "published"),
+)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField(blank=True, default="")
+    word_count = models.IntegerField()
+    twitter_post = models.TextField(blank=True, default="")
+    status = models.CharField(max_length=20,
+                              choices=ARTICLE_STATUS,
+                              default="draft"
+                              )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
