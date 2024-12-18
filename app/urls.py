4883fa682abe44b2ba6ea12da4e7a95625ee3e7f
silaspaul ikedi <ikedichukwu1993@gmail.com>
2024-12-18 12:16:23 +0200

    cleaned up our urls

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
-    path("articles/create/", ArticleCreateView.as_view(), name="create_article"),
-    path("articles/<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
-    path("articles/<int:pk>/delete/", ArticleDeleteView.as_view(), name="update_article")
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
+    path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
+    path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="update_article")
 ]
