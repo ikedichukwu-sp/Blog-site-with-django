0de332c5007860390c3ad1d9c9ccc045eb80933f
silaspaul ikedi <ikedichukwu1993@gmail.com>
2025-01-31 21:40:17 +0200

    setup postgreSQl connections with psycopg, run     django app and database with docker compose

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,10 +113,7 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600)  # DATABASE_URL
 }
 
 # Customize user model
